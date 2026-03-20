ec98784d96cd07e2c704ff9d8c1bb3d9cf36c2ea
GSNFonseka <nowanyafonseka@gmail.com>
2026-03-20 23:45:48 +0530

    Add slow_mo and click Docs link

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     # Launch a browser.
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     # Create a new page.
     page = browser.new_page()
     # Open the Playwright Python docs.
     page.goto("https://playwright.dev/python")
+
+    #locate a link element with "Docs" text 
+    docs_button = page.get_by_role('link',name="Docs")
+    docs_button.click()
 
     browser.close()
